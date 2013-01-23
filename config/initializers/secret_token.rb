b947f24b67954543b7cd39f2587e2a2a6bfdb3d2
Lane Halley <lane@carbonfive.com>
2013-01-23 15:05:39 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testproj::Application.config.secret_token = '025afdeb8a7d8438b2402047fdf932d2a88c4b31ab01b1a5c9b50e259d2e86c5d99f2e069e72aac5daad079eec95daab93204695027b3b3a648add264d389963'
